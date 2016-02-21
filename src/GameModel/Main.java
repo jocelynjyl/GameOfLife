8a2f69a3ddcda21fc156b0cac3b8dd44a2375aa2
jocelynjyl <jocelynjyl@gmail.com>
2016-02-20 21:23:33 -0800

    Update to IntelliJ project structures

--- src/GameModel/Main.java
+++ src/GameModel/Main.java
@@ -1,6 +1,5 @@
 package GameModel;
 
-import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,16 +8,12 @@ public class Main
 	public static void main (String[] args)
 	{
         Grid life = new Grid (3, 9);
-        JFrame frame = new JFrame("Game of Life");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        frame.getContentPane();
-        frame.pack();
-        frame.setVisible(true);
 
 		life.setInitial (1,0);
 		life.setInitial(1,1);
 		life.setInitial (1,2);
 
+        life.nextGeneration();
+        life.printGame();
     }
 }
